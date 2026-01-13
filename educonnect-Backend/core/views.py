b2ac68638b35947220f002c9aeed9fb325a52ff6
Jason <152582224+JEX2009@users.noreply.github.com>
2026-01-13 16:27:37 -0600

    feat: Creacion del esqueleto del backend y las cookies http

---
+++ educonnect-Backend/core/views.py
@@ -0,0 +1,37 @@
+#Aca solo ira esta vista que es la que inyecta el token a la cookie y el eliminarlo
+#Las otras vistas van en apps aparte
+from django.conf import settings
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class ObtencionTokens(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        
+        if response.status_code == 200:
+            access_token = response.data.get('access')
+            refresh_token = response.data.get('refresh')
+
+            response.set_cookie(
+                key=settings.SIMPLE_JWT['AUTH_COOKIE'],
+                value=access_token,
+                expires=settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'],
+                secure=settings.SIMPLE_JWT['AUTH_COOKIE_SECURE'],
+                httponly=settings.SIMPLE_JWT['AUTH_COOKIE_HTTP_ONLY'],
+                samesite=settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
+            )
+            
+            del response.data['access']
+            
+        return response
+
+class EliminacionTokens(APIView):
+    def post(self, request):
+        response = Response({"message": "Sesión cerrada correctamente"}, status=status.HTTP_200_OK)
+        
+        response.delete_cookie(settings.SIMPLE_JWT['AUTH_COOKIE'])
+        response.delete_cookie(settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH'])
+        
+        return response
