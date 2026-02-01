44cc514d0b56b9198249128cc8ad664b1e450aac
sabsUG <107018722+sabsUG@users.noreply.github.com>
2026-01-31 20:59:32 -0600

    linea de conflicto borrada

--- educonnect-Backend/apps/databaseModels/apps.py
+++ educonnect-Backend/apps/databaseModels/apps.py
@@ -5,6 +5,5 @@ class DatabasemodelsConfig(AppConfig):
     name = 'apps.databaseModels'
     label = 'databaseModels' 
 
->>>>>>> 3daec9997c053b23e151a64b5c15781e53491c11
     def ready(self):
         import apps.databaseModels.signals
